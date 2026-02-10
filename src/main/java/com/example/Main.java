e3cf2d8d030fe8db200fc2ab36b97d8fd4fa6883
Alexandrine Dubé <alexandrine.dube@hotmail.com>
2026-02-10 04:24:47 -0500

    correction du main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,10 +6,9 @@ public class Main {
     }
 
     public static void showMenu() {
-        SnowmobileProduction SnowCompany = new SnowmobileProduction();
-        PianoProduction ClassiqueCompany = new PianoProduction();
-        SpeakersProduction MusicCompany = new SpeakersProduction();
+        
         while (true) {
+            System.out.println("\n=== MENU PRINCIPAL ===");
             System.out.println("Que voulez-vous faire ?");
             System.out.println("1. Produire un objet");
             System.out.println("2. Quitter");
@@ -18,26 +17,7 @@ public class Main {
             switch (choix) {
                 case "1" -> {
                     System.out.println("Vous avez choisi de produire un objet");
-                    System.out.println("Quelle est votre entreprise ?");
-                    System.out.println("1. SnowCompany");
-                    System.out.println("2. PianoForAll");
-                    System.out.println("2. MusicEverywhere");
-                    String company = System.console().readLine();
-                    switch (company) {
-                        case "1" -> {
-                            System.out.println("Vous avez choisi SnowCompany");
-                            SnowCompany.createObject();
-                        }
-                        case "2" -> {
-                            System.out.println("Vous avez choisi PianoForAll");
-                            ClassiqueCompany.createObject();
-                        }
-                        case "3" -> {
-                            System.out.println("Vous avez choisi MusicEverywhere");
-                            MusicCompany.createObject();
-                        }
-                        default -> System.out.println("Vous avez choisi une entreprise invalide");
-                    }
+                    Production.produceObject();
                 }
                 case "2" -> {
                     System.out.println("Vous avez choisi de quitter");
